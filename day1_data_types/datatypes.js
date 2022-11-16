42b861a64cf8ae0aa7ad0d54708345fd73f0125b
jschaefmn <jschaefmn@gmail.com>
2022-11-16 09:40:55 -0600

    finished day 2 and renamed a folder

---
+++ day1_data_types/datatypes.js
@@ -0,0 +1,19 @@
+// Declare second integer, double, and String variables.
+let i2;
+let d2;
+let s2;
+
+// Read and save an integer, double, and String to your variables.
+i2 = Number(input_stdin_array[0]);
+d2 = Number(input_stdin_array[1]);
+s2 = input_stdin_array[2];
+
+// Print the sum of both integer variables on a new line.
+console.log(i2 + i);
+
+// Print the sum of the double variables on a new line.
+console.log((d2 + d).toFixed(1));
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+console.log(s + s2);
