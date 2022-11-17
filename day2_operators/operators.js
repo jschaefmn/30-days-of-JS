da6f0ea64df2b4ffc1d63a0e48f9150b979e9f26
jschaefmn <jschaefmn@gmail.com>
2022-11-17 11:35:37 -0600

    finished day 2

---
+++ day2_operators/operators.js
@@ -0,0 +1,13 @@
+function solve(meal_cost, tip_percent, tax_percent) {
+  // Write your code here
+  //find tip_percent
+  const meal_tip = meal_cost / 100 * tip_percent;
+  
+  //find tax percent
+  const meal_tax = tax_percent / 100 * meal_cost;
+  
+  // find meal total
+  const meal_total = Math.floor(meal_cost + meal_tip + meal_tax);
+  
+  console.log(meal_total);
+}
